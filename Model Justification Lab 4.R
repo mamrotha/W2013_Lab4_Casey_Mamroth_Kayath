39b7d929acabfa11735a9dd891049d1a615a62e7
mamrotha <andrew_mamroth@berkeley.edu>
2017-08-19 10:46:24 -0400

    Started a draft and began justification of variables

--- Model Justification Lab 4.R
+++ Model Justification Lab 4.R
@@ -12,8 +12,9 @@ summary(model_f)
 #then continuing adding additional independant variables that add the most value.
 #In this case, density is by far the most well correlated variable and we start
 #there.
+Model1_r<-summary(lm(crime$crmrte~crime$density))$adj.r.squared
 lapply(crime[5:26], function(x) 
-  summary(lm(crime$crmrte~crime$density+x))$adj.r.squared)
+  summary(lm(crime$crmrte~crime$density+x))$adj.r.squared-Model1_r)
 
 #From here the variable pctmin80 adds to most predictive power to the model. 
 #Also important to note is that the western counties apparently have a higher
@@ -41,26 +42,63 @@ lapply(crime[5:26], function(x)
   summary(lm(crime$crmrte~crime$density+crime$prbarr+
                +crime$prbconv+crime$polpc+crime$pctmin80+x))$adj.r.squared)
 
-#From here, no variable adds any more value than 0.6 which we deem to be 
+#we also check that we can't remove any variables
+lapply(crime[5:26], function(x) 
+  summary(lm(crime$crmrte~crime$density+crime$prbarr+
+               +crime$prbconv+crime$polpc+crime$pctmin80-x))$adj.r.squared)
+
+#From here, no variable adds any more value than 0.006 which we deem to be 
 #insignificant from here.
 
 #Final
-dat_f<-data.frame(crmrte,density,prbarr,prbconv,polpc,min80)
+
+dat_f<-data.frame(crime$crmrte,crime$density,crime$prbarr,
+                  crime$prbconv,crime$polpc,crime$pctmin80)
 cor(dat_f)
 
 #Test Stuff
-lapply(crime[5:26], function(x) 
-  summary(lm(crime$crmrte~crime$polpc+crime$pctmin80+crime$prbconv
-             +crime$prbarr+x))$adj.r.squared)
+lapply(crime[5:26], function(x) cor(crime$prbarr,x))
+
+mod_sum<-summary(lm(crime$crmrte~crime$density+crime$prbconv+
+                      crime$prbarr+crime$polpc+
+                      crime$pctmin80-crime$pctmin80))
+adjsq<-mod_sum$adj.r.squared
+check<-mod_sum$residual
+lapply(crime[5:26], function(x) cor(check,x))
+check
+adjsq
+
+AIC(model_f)
+AIC(lm(crime$crmrte~crime$density))
 
 lapply(crime[5:26], function(x) 
-  summary(lm(crime$crmrte~crime$density+crime$polpc+x))$adj.r.squared)
+  AIC(lm(crime$crmrte~crime$density+crime$prbarr+crime$pctmin80+
+           crime$polpc+x)))
 
-lapply(crime[4:26], function(x) cor(crime$prbconv,x))
-lapply(crime[4:26], function(x) cor(crime$wser,x))
+summary(lm(log(crime$crmrte)~crime$prbarr+crime$prbconv+crime$prbpris+crime$avgsen
+           +crime$polpc+log(crime$density)+crime$taxpc+crime$west+crime$central
+           +crime$urban+crime$pctmin80+crime$wcon+crime$wtuc+crime$wtrd+crime$wfir
+           +crime$wser+crime$wmfg+crime$wfed+crime$wsta+crime$wloc+crime$mix
+           +crime$pctymle))
 
-west<-crime$west
-dat_t<-data.frame(crmrte,density,prbarr,prbconv,polpc,min80,west)
-cor(dat_t)
-cor(taxpc,polpc)
-cor(west, density)
+summary(lm(log(crime$crmrte)~log(crime$density)+crime$prbarr
+             +crime$prbconv+log(crime$polpc)+crime$pctmin80))
+
+
+summary(lm(crime$crmrte~crime$prbarr+crime$prbconv+crime$prbpris+crime$avgsen
+           +crime$polpc+crime$taxpc+crime$west+crime$central
+           +crime$urban+crime$pctmin80+crime$wcon+crime$wtuc+crime$wtrd+crime$wfir
+           +crime$wser+crime$wmfg+crime$wfed+crime$wsta+crime$wloc+crime$mix
+           +crime$pctymle))
+
+summary(lm(crime$wfed~crime$prbarr+crime$prbconv+crime$prbpris+crime$avgsen
+           +crime$polpc+crime$density+crime$taxpc+crime$west+crime$central
+           +crime$urban+crime$pctmin80+crime$wcon+crime$wtuc+crime$wtrd
+           +crime$wfir+crime$wser+crime$wmfg+crime$crmrte+crime$wsta+crime$wloc
+           +crime$mix+crime$pctymle))
+
+redsid<-summary(lm(crime$crmrte~crime$density))$residual
+lapply(crime[5:26], function(x) cor(resid,x))
+
+summary(lm(crime$crmrte~crime$wcon+crime$wtuc+crime$wtrd+crime$wfir
+           +crime$wser+crime$wmfg+crime$wfed+crime$wsta+crime$wloc))
